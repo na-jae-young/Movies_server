c67b87393923cfb5e2c148f0b5ec5a1fb113b8d4
DESKTOP-ULAPNNB\hr <tprtnlzkwlsh@naver.com>
2022-11-03 15:03:35 +0900

    배포제발

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
+const config = require('./config')
 
 const db = mysql.createConnection({
-    host     : "127.0.0.1",
-    user     : "nah0101" ,
-    port: 3306,
-    password : 'cjdruf0984~',
-    database : 'crud',
+    host     : config.host,
+    user     : config.user,
+    port: config.port,
+    password : config.password,
+    database : config.database,
     insecureAuth: true,
     dateStrings:"date" // mysql 에서 시간 갖어올때 
 });
